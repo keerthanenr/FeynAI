2bc2835f63a3f66d3592a876428c9346b3146e9b
davidmeadejr <davidmeadejnr@gmail.com>
2024-11-26 01:49:26 +0000

    updated header

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="flex flex-col items-center gep-2 py-4">
       <h1 className="text-4xl font-bold">FeynAI</h1>
-      <p className="text-lg text-gray-600">If you know you know.</p>
+      <p className="text-lg text-gray-600">"If you want to master something, teach it."</p>
     </header>
   );
 }
