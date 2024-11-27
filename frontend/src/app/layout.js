34999833751ed0342681325ec5a8e55211bd56b9
davidmeadejr <davidmeadejnr@gmail.com>
2024-11-27 00:03:29 +0000

    updated global font

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,14 +1,9 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const helveticaNueueMedium = localFont({
+  src: "./fonts/HelveticaNeueMedium.woff",
+  variable: "--font-helvetica-neue-medium",
   weight: "100 900",
 });
 
@@ -21,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${helveticaNueueMedium.variable} antialiased`}
       >
         {children}
       </body>
